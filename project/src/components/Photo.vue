d17c10fef034ee610757379dedeb509bbcbbb58e
Camila Micaela Urquizo Suca <curquizo@belatrix.com>
2020-03-05 15:24:35 -0500

    Init

---
+++ project/src/components/Photo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="photo-profile">
+        <img src="../assets/profile.jpeg">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Photo'
+ 
+}
+</script>
+
+<style lang="less" scoped>
+.photo-corners (@radius: 50%){
+    border-radius: @radius;
+    width: 300px;
+    height: 300px;
+}
+.photo-profile{
+ .photo-corners;
+}
+.photo-profile >img {
+ .photo-corners;
+}
+</style>
---
+++ project/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+     <div> 
+      <Photo/>
+      <h1>Welcome to my Page</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Photo from '@/components/Photo.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Photo
+  }
+}
+</script>
